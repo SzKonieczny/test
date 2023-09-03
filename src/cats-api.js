b8af1be0aa0ddb87d8dc0b284e9e4755d2e59164
Szymon Konieczny <szymon_konieczny@o2.pl>
2023-09-03 02:48:46 +0200

    cats-api.js added

---
+++ src/cats-api.js
@@ -0,0 +1,47 @@
+import axios from 'axios';
+axios.defaults.headers.common['x-api-key'] =
+  'live_V4svR7tPTorAPN1vQiuugtos4Ngifb7vLiPEy0XOEJBIB1CcI6DJnkBbxEoLSF0c';
+
+export function fetchBreeds() {
+  return axios
+    .get('https://api.thecatapi.com/v1/breeds')
+    .then(response => {
+      return response.data;
+    })
+    .catch(error => {
+      // new Error('Oops! Something went wrong! Try reloading the page!');
+      errorInAxios(error);
+    });
+}
+
+export function fetchCatByBreed(breedId) {
+  return axios
+    .get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
+    .then(response => {
+      console.log(response.data);
+      return response.data;
+    })
+    .catch(error => {
+      errorInAxios(error);
+      // new Error('Oops! Something went wrong! Try reloading the page!');
+    });
+}
+
+function errorInAxios(error) {
+  if (error.response) {
+    // The request was made and the server responded with a status code
+    // that falls out of the range of 2xx
+    console.log(error.response.data);
+    console.log(error.response.status);
+    console.log(error.response.headers);
+  } else if (error.request) {
+    // The request was made but no response was received
+    // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+    // http.ClientRequest in node.js
+    console.log(error.request);
+  } else {
+    // Something happened in setting up the request that triggered an Error
+    console.log('Error', error.message);
+  }
+  console.log(error.config);
+}
